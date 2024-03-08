78eb0106ed1e0b9ac107494e4167f05d5959b969
AfyQvoa <noiteazul@outlook.com>
2024-03-08 17:57:45 -0300

    Aham

--- L6 E2.js
+++ L6 E2.js
@@ -1,1 +1,11 @@
-console.log(`Hello World`);
+
+
+function verificadorParImpar(num) {
+    if(num % 2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(verificadorParImpar);
